3aefd3b28d5d775372d902d21f659b352dcb8568
baek <xemeal@gmail.com>
2021-01-26 22:04:42 +0900

    slack commands 모듈화 작업

--- src/controllers/redis-controller.js
+++ src/controllers/redis-controller.js
@@ -13,21 +13,46 @@ exports.getData = async(req, res, next)=>{
     }
 }
 
+exports.commands = async(req, res, next)=>{
+  let body = req.body;
+  let event = body.event;
+  let text = body.text;
+    try {
+      console.log("================");
+      let commands = text.split("@");
+      console.log("[commands]text:" +commands[0]);
+      console.log("[commands]text:" +text);
+
+      // commands종류에 따라서 분기
+      switch(commands[0]){
+        case "set":
+          let set_key = commands[1];
+          let value = commands[2];
+          // let expire = commands[3];
+          redisService.set(set_key,value);
+          break;
+
+        case "get":
+            let get_key = commands[1];
+            redisService.get(get_key,value);
+          break;
+
+      }
+      res.sendStatus(200);
+    } catch (error) {
+        console.log("[error]: " +error)
+        res.status(500).json(err)
+    }
+}
+
 exports.setData = async(req, res, next)=>{
   
   let body = req.body;
   let command = body.command;
   let text = body.text;
   try {
-    console.log("================");
-    console.log("command: "+ command);
-    console.log("text: "+ text);
-    console.log("================");
 
-    redisService.rPush("lunchList",text)
-    
-
-    
+    redisService.set("lunchList",text)
     res.sendStatus(200);
   
   } catch (error) {

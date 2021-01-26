561127657c43f35ac86704546e77852c2d8afc3a
baek <xemeal@gmail.com>
2021-01-26 21:45:23 +0900

    Cache store기능 추가

--- src/services/redis.js
+++ src/services/redis.js
@@ -6,30 +6,80 @@ const client = redis.createClient({
     port:6379,
 })
 
-let instance = class {
+client.on("error", (err) => {
+    console.log("[Err] "+ err);
+});
+client.on("ready", () => {
+    console.log("Redis is ready ");
+});
 
-    get(key){
-        return client.get(key, (err, result) => {
-            return result;
-        });        
-    }
-    set(key, value){
-        client.set(key, value);
-    }
-    // 마지막 list에 추가
-    rPush(key, value) {
-        client.rpush(key, value); 
-        // client.rpush(1, 2); 
-    }
-    //list 처음에 추가
-    lPush(key, value) {
-        client.lpush(key, value);
-    }
-    // start~end까지의 list검색
-    lRange(key, start, end) {
-        return client.lrange(key, start, end, (err, result) => {
-            return result;
+let instance = class {
+    set(slackId, token, expireTime){
+        try {
+            console.log("[redis.expire]start");
+            console.log("[redis.set]slackId :" + slackId+", token: "+token);
+            client.set(slackId, token);
+            if(expireTime) {
+                this.expire(slackId, expireTime);
+            }    
+        } catch (error) {
+            throw(error);
+        }
+    };
+
+    get(slackId){
+        return new Promise((resolve, reject) => {
+            try {
+                client.get(slackId, (err, result) => {
+                    if(err){
+                        reject(resolve);
+                    }
+                    console.log("[redis.get]start");
+                    console.log("[redis.get]slackId :" + slackId+", token: "+result);
+                    resolve(result);
+                });
+            } catch (error) {
+                throw(error)
+            }
         });
+    };
+    
+    expire(slackId, expireTime) {
+        try {
+            console.log("[redis.expire]start");
+            console.log("[redis.expire]slackId :" + slackId+", expireTime: "+expireTime);
+            client.expire(slackId, expireTime);
+        } catch (error) {
+            throw(error)
+        }
+    }
+
+    exists(slackId){
+        return new Promise((resolve, reject) => {
+            client.exists(slackId, (err, result) => {
+                if(err){
+                    reject(resolve);
+                }
+                const isExists = result == "1"? true:false;
+                resolve(isExists);
+            });
+        });
+    };
+
+    delete(key) {
+        try {
+            console.log("[redis.delete]start");
+            console.log("[redis.delete]key :" + key);
+            client.del(key);
+        } catch (error) {
+            throw(error)
+        }
+    }
+
+    rename(originKey, rename) {
+        console.log("[redis.rename]start");
+        console.log("[redis.rename]originKey :" + originKey+", rename: "+rename);
+        client.rename(originKey, rename);
     }
 
 }
